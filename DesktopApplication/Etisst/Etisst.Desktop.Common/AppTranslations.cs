3d2d43f2341f63c23e8e6c86f972fe6ea2f1a8de
Alin Craciun <craciun96alin@gmail.com>
2019-05-13 14:07:25 +0300

    Set environment

---
+++ DesktopApplication/Etisst/Etisst.Desktop.Common/AppTranslations.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Etisst.Desktop.Common
+{
+    [System.Runtime.InteropServices.GuidAttribute("76DF2981-3387-429E-8AD2-BEBFB4C79132")]
+
+    public class AppTranslations
+    {
+
+    }
+}
---
+++ DesktopApplication/Etisst/Etisst/Forms/frmStartForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Etisst
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mpMainMenu.MouseClick += MpMainMenu_MouseClick;
+        }
+
+        #region Events
+        private void MpMainMenu_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.mpMainMenu.AutoSize = true;
+        }
+
+        private void mtHome_Click(object sender, EventArgs e)
+        {
+
+            this.mpMainMenu.AutoSize = true;
+            this.mtHome.TabIndex = 2;
+        }
+        #endregion
+    }
+}
